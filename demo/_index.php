adcc5f0b01a33b7c4b339c1fca4568fb5fc0e35b
kamei@workspot.jp <asao@asaoMBAir.local>
2012-01-18 22:45:04 +0900

    added a routes info for Router.

--- demo/_index.php
+++ demo/_index.php
@@ -5,11 +5,17 @@ require_once( __DIR__ . '/../src/AmidaMVC/bootstrap.php');
  * TODO: demo as test site.
  *  before eating dog food, make test/demo site.
  */
-//class Debug extends AmidaMVC\Component\Debug {}
+
+$routes = array(
+    '/route/:action' => array( 'file' => 'route/_App.php', 'action' => 'index' ),
+);
+AmidaMVC\Component\Router::set( $routes );
+
 $data = array();
 $ctrl = new AmidaMVC\Framework\Controller();
 $ctrl
     ->loadDebug()
+    ->addModel( 'Router', 'route' )
     ->addModel( 'Loader', 'load' )
     ->addModel( 'Viewer', 'view' )
 ;
