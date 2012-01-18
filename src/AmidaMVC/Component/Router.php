1d6edcb02ca8c7ca627e2b060c53a9170ac6be8b
kamei@workspot.jp <asao@asaoMBAir.local>
2012-01-18 22:41:04 +0900

    initial Router class based on Perfect PHP book.

---
+++ src/AmidaMVC/Component/Router.php
@@ -0,0 +1,52 @@
+<?php
+namespace AmidaMVC\Component;
+
+class Router
+{
+    // +-------------------------------------------------------------+
+    /**
+     * @var array   contains pattern to parameter array.
+     */
+    private static $_routes = array();
+    // +-------------------------------------------------------------+
+    static function set( $routes ) {
+        self::$_routes = self::compile( $routes );
+    }
+    // +-------------------------------------------------------------+
+    static function compile( $routes ) {
+        $_routes = array();
+        foreach( $routes as $url => $match ) {
+            $tokens = explode( '/', ltrim( $url, '/' ) );
+            foreach( $tokens as $i => $token ) {
+                if( strpos( $token, ':' ) === 0 ) {
+                    $name  = substr( $token, 1 );
+                    $token = "(?P<{$name}>[^/]+)";
+                }
+                $tokens[$i] = $token;
+            }
+            $pattern = '/' . implode( '/', $tokens );
+            $_routes[ $pattern ] = $match;
+        }
+        return $_routes;
+    }
+    // +-------------------------------------------------------------+
+    static function match( $path ) {
+        if( substr( $path, 0, 1 ) !== '/' ) {
+            $path = '/' . $path;
+        }
+        foreach( self::$_routes as $pattern => $match ) {
+            if( preg_match( "#^{$pattern}$#", $path, $matches ) ) {
+                $match = array_merge( $match, $matches );
+            }
+            return $match;
+        }
+        return FALSE;
+    }
+    // +-------------------------------------------------------------+
+    static function actionDefault( $ctrl, &$data ) {
+        $ctrl->debug( 'wordy', $ctrl->path, 'ctrl path' );
+        self::match( $ctrl->path );
+        $data .= 'Router::default';
+    }
+    // +-------------------------------------------------------------+
+}
