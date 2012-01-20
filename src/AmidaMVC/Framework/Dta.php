80169c5ecfea951000fc95e62ec52f0fb9295d9a
Asao Kamei <kamei@workspot.jp>
2012-01-20 13:42:21 +0900

    class for data-transfer-object, to hold data during the chain.

---
+++ src/AmidaMVC/Framework/Dta.php
@@ -0,0 +1,52 @@
+<?php
+namespace AmidaMVC\Framework;
+
+/**
+ * Data Transfer Object.
+ */
+class Dto
+{
+    /**
+     * @var array   contains _data to transfer.
+     */
+    var $_data = array();
+    // +-------------------------------------------------------------+
+    function __construct() {
+        $this->_data = array();
+    }
+    // +-------------------------------------------------------------+
+    //  setter and getter
+    // +-------------------------------------------------------------+
+    /**
+     * setter for $name. stores at $this->_data[$name].
+     * @param type $name   name of value to set.
+     * @param type $value  the value.
+     * @return $this.
+     */
+    function set( $name, $value ) {
+        if( is_array( $name ) ) {
+            $this->_data = array_merge( $this->_data, $name );
+        }
+        else {
+            $this->_data[ $name ] = $value;
+        }
+        return $this;
+    }
+    // +-------------------------------------------------------------+
+    /**
+     * getter for $name. if $name is not given, return all the _data.
+     * @param string $name name of value to get.
+     * @return mix        returns the named value,
+     *                     or all _data if name is not given.
+     */
+    function get( $name=NULL ) {
+        if( $name === NULL ) {
+            return $this->_data;
+        }
+        if( isset( $this->_data[ $name ] ) ) {
+            return $this->_data[ $name ];
+        }
+        return FALSE;
+    }
+    // +-------------------------------------------------------------+
+}
