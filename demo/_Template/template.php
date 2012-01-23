f3b043589b5e4a22f9e0a3369fef03417232fb17
Asao Kamei <kamei@workspot.jp>
2012-01-23 09:58:19 +0900

    a little improvement on template design.

--- demo/_Template/template.php
+++ demo/_Template/template.php
@@ -10,20 +10,42 @@
             padding-left:15px;
             border-radius: 15px;
             font-family:"ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", "メイリオ", Meiryo, Osaka, "ＭＳ Ｐゴシック", "MS PGothic", sans-serif;
-            box-shadow: 5px 5px 5px rgba(0,0,0,0.25);
+            box-shadow: 5px 5px 5px #aaaaaa;
+        }
+        header {
+            background: #F0F0F0;
+            font-size: 18px;
+            font-weight: bold;
+            text-align: center;
+            padding: 15px 5px 15px 5px;
+            margin: 0px 0px 10px -5px;
+            border-left: 5px solid #d2691e;
+            border-right: 5px solid #d2691e;
+            box-shadow: 5px 5px 5px #cccccc;
+        }
+        footer {
+            background: #F0F0F0;
+            font-size: 10px;
+            font-weight: bold;
+            text-align: center;
+            padding: 5px 5px 5px 5px;
+            margin: 0px 0px 0px -5px;
+            border-left: 2px solid #d2691e;
+            border-right: 2px solid #d2691e;
+            box-shadow: 2px 2px 2px #cccccc;
         }
         h1 {
             background: #F0F0F0;
-            font-size: 18px;
-            padding: 15px 5px 15px 5px;
-            border-left: 5px solid #d2691e;
-            box-shadow: 5px 5px 5px rgba(0,0,0,0.25);
-            margin-left: -5px;
+            font-size: 16px;
+            padding: 5px 5px 5px 5px;
+            margin: 15px 5px 5px -2px;
+            border-left: 2px solid #d2691e;
+            box-shadow: 3px 3px 3px #cccccc;
         }
         table {
             border: #CCCCCC solid 2px;
             border-collapse:collapse;
-            box-shadow: 2px 2px 2px rgba(0,0,0,0.25);
+            box-shadow: 2px 2px 2px #cccccc;
         }
         th {
             background-color: #F0F0F0;
@@ -37,7 +59,7 @@
     </style>
 </head>
 <body>
-<header>AmidaMVC Demo Site</header>
+<header>AmidaMVC Demo</header>
 <div id="contents">
     <h1><?php echo $title; ?></h1>
     <?php echo $contents; ?>
