4a22db1fbdb2b758ad4d1e6763d9bbd6a8bfffd6
Asao Kamei <kamei@workspot.jp>
2012-01-23 11:57:11 +0900

    test Debug in application. works if Debug is loaded as a component.

--- demo/app1/_App.php
+++ demo/app1/_App.php
@@ -3,11 +3,12 @@
 class modelApp1
 {
     static function actionDefault( $ctrl, &$data ) {
+        Debug::bug( 'head', 'app1Model::Default action' );
         $data->setContents( "app1Model::Default, action=".$ctrl->currAct() );
         $ctrl->nextModel( 'pageNotFound' );
     }
     static function actionList( $ctrl, &$data ) {
-        //
+        Debug::bug( 'head', 'app1Model::List action' );
         $data->setContents( "Model::List action=".$ctrl->currAct() );
     }
 }
