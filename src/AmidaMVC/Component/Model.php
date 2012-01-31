06444ef4048b404d9714770cc9389efdeb71649b
Asao Kamei <kamei@workspot.jp>
2012-01-31 16:44:32 +0900

    added setRestMethod for dubm rest module.

--- src/AmidaMVC/Component/Model.php
+++ src/AmidaMVC/Component/Model.php
@@ -8,5 +8,31 @@ class Model
 
     }
     // +-------------------------------------------------------------+
+    /**
+     * a dumb setup routine for Rest. 
+     * creates actionAction_Method like 'actionResource_put'.   
+     * @static
+     * @param \AmidaMVC\Framework\Controller $ctrl
+     * @param SiteObj $siteObj
+     */
+    static function setRestMethod(
+        \AmidaMVC\Framework\Controller $ctrl,
+        \AmidaMVC\Component\SiteObj &$siteObj )
+    {
+        $methods = array( '_post', '_put', '_delete', '_edit', '_new', '_get' );
+        $siteInfo = $siteObj->get( 'siteObj' );
+        $restMethod = FALSE;
+        foreach( $methods as $method ) {
+            if( in_array( $method, $siteInfo[ 'command' ] ) ) {
+                $restMethod = $method;
+            }
+        }
+        if( $restMethod ) {
+            $action = $ctrl->getAction();
+            $action .= $restMethod;
+            $ctrl->setAction( $action );
+        }
+    }
+    // +-------------------------------------------------------------+
 }
 
