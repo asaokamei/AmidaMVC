f3e4e22d5c176a061e3e90465cefe8c26ef0ec3e
Asao Kamei <kamei@workspot.jp>
2012-02-06 15:09:10 +0900

    removed debug info from normal template.

--- _Config/template.php
+++ _Config/template.php
@@ -15,32 +15,5 @@
     <p style="clear: both;"></p>
 </div>
 <footer>AmidaMVC, yet another micro Framework for PHP.</footer>
-<?php if( !empty( $debug ) ) { ?>
-<style>
-    div.debugInfo {
-        font-size: 12px;
-        color: #666666;
-        margin: 10px;
-        padding: 5px 5px 5px 25px;
-        border: 1px solid #cccccc;
-        box-shadow: 2px 2px 2px #cccccc;
-    }
-    div.debugInfo h3 {
-        margin: 0px;
-        padding: 2px;
-    }
-    div.debugInfo .debugTraceInfo {
-        float: left;
-        margin: 0px 0px 0px -20px;
-    }
-    div.debugInfo table {
-        border:1px solid gray; font-size: 11px; border-collapse: collapse;
-    }
-    div.debugInfo td,th { border: 1px dotted gray; vertical-align: top; }
-    div.debugInfo th { background-color: #E0E0E0; }
-</style>
-<div class='debugInfo'><?php echo $debug;?></div>
-</div>
-    <?php } ?>
 </body>
 </html>
