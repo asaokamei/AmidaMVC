3bc344188f43f7045746e6ff68bab0cd43969747
Asao Kamei <kamei@workspot.jp>
2012-02-12 13:04:34 +0900

    reformated source code by PHPStorm.

--- _Config/template._dev.php
+++ _Config/template._dev.php
@@ -1,48 +1,50 @@
 <?php
 
 // filer's mode; _edit, _put, _pub, _del,...
-$file_mode = ( isset( $_siteObj->filerObj->file_mode ) ) ? $_siteObj->siteObj->file_mode: FALSE;;
+$file_mode = (isset($_siteObj->filerObj->file_mode)) ? $_siteObj->siteObj->file_mode : FALSE;
+;
 // myself.
 $self = $_ctrl->getBaseUrl() . $_ctrl->getPathInfo();
-    
+
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
+    <meta charset="UTF-8"/>
     <title><?php echo $head_title; ?></title>
-    <link rel="stylesheet" href="<?php echo $_siteObj->siteObj->base_url; ?>/demo.css" />
+    <link rel="stylesheet" href="<?php echo $_siteObj->siteObj->base_url; ?>/demo.css"/>
 </head>
 <body>
 <header><a href="<?php echo $_ctrl->getBaseUrl(); ?>">AmidaMVC Framework</a></header>
 <div id="contents">
-    <?php if( $title ) { ?>
+    <?php if ($title) { ?>
     <h1><?php echo $title; ?></h1>
     <?php } ?>
-    <?php 
-    if( !isset( $_siteObj->filerObj ) ) {
+    <?php
+    if (!isset($_siteObj->filerObj)) {
         echo $contents;
     }
     else
-    if( $_siteObj->filerObj->file_mode == '_edit' ) {
-        // show form to edit contents.
-?>
-        <form method="post" name="_editFile" action="<?php echo $self?>/_put">
-            <textarea name="_putContent" style="width:95%; height:350px; font-family: courier;"><?php echo htmlspecialchars( $contents ); ?></textarea>
-            <input type="submit" name="submit" value="put contents"/>
-        </form>
-<?php 
-    }
-    else {
-        echo $contents;
-    }
+        if ($_siteObj->filerObj->file_mode == '_edit') {
+            // show form to edit contents.
+            ?>
+            <form method="post" name="_editFile" action="<?php echo $self?>/_put">
+                <textarea name="_putContent"
+                          style="width:95%; height:350px; font-family: courier;"><?php echo htmlspecialchars($contents); ?></textarea>
+                <input type="submit" name="submit" value="put contents"/>
+            </form>
+            <?php
+        }
+        else {
+            echo $contents;
+        }
     ?>
     <p style="clear: both;"></p>
 </div>
 <footer>AmidaMVC, yet another micro Framework for PHP.</footer>
 <?php
-  if( isset( $_siteObj->filerObj ) ): 
-?>
+if (isset($_siteObj->filerObj)):
+    ?>
 <style>
     div#filerMenu {
         margin: 10px;
@@ -50,28 +52,33 @@ $self = $_ctrl->getBaseUrl() . $_ctrl->getPathInfo();
         border: 1px solid #cccccc;
         box-shadow: 2px 2px 2px #cccccc;
     }
+
     div#filerMenu ul {
-        margin:0px;
-        padding:0px;
+        margin: 0px;
+        padding: 0px;
         list-style-type: none;
         height: 25px;
     }
+
     div#filerMenu li {
         float: left;
-        margin: 2px 5px 2px 5px; 
+        margin: 2px 5px 2px 5px;
     }
+
     div#backUpList {
         margin: 10px;
         padding: 5px 5px 5px 25px;
         border: 1px solid #cccccc;
         box-shadow: 2px 2px 2px #cccccc;
     }
+
     div#filerError {
         margin: 10px;
         padding: 2px 2px 2px 25px;
         border: 2px solid #d59392;
         box-shadow: 3px 3px 3px #d59392;
     }
+
     div#filerError dt {
         font-weight: bold;
         color: red;
@@ -79,60 +86,72 @@ $self = $_ctrl->getBaseUrl() . $_ctrl->getPathInfo();
 </style>
 <div id="filerDivArea">
 <div id="filerMenu">
-  <p>source_type: <?php echo $_siteObj->filerObj->src_type;?></p>
-  <ul>
-  <?php foreach( $_siteObj->filerObj->file_cmd as $cmd ) { ?>
-    <li><a href="<?php echo "$self/$cmd";?>" ><?php echo $cmd;?></a> </li>
+    <p>source_type: <?php echo $_siteObj->filerObj->src_type;?></p>
+    <ul>
+        <?php foreach ($_siteObj->filerObj->file_cmd as $cmd) { ?>
+        <li><a href="<?php echo "$self/$cmd";?>"><?php echo $cmd;?></a></li>
+        <?php } ?>
+    </ul>
+</div>
+    <?php if (isset($_siteObj->filerObj->error)) { ?>
+    <div id="filerError">
+        <dl>
+            <dt>Error: <?php echo $_siteObj->filerObj->error; ?></dt>
+            <dd><?php echo $_siteObj->filerObj->err_msg; ?></dd>
+        </dl>
+    </div>
     <?php } ?>
-  </ul>
-</div>
-<?php if( isset( $_siteObj->filerObj->error ) ) { ?>
-<div id="filerError">
-    <dl>
-        <dt>Error: <?php echo $_siteObj->filerObj->error; ?></dt>
-        <dd><?php echo $_siteObj->filerObj->err_msg; ?></dd>
-    </dl>
-</div>
-<?php }  ?>
-<?php if( !empty( $_siteObj->filerObj->backup_list ) ) { ?>
-<div id="backUpList">
-  <ul>
-    <?php foreach( $_siteObj->filerObj->backup_list as $backup ) { ?>
-    <li>
-        <?php echo $backup;?>: 
-        [<a href="<?php echo "{$self}/_bkView:{$backup}";?>" >view</a>]
-        [<a href="<?php echo "{$self}/_bkDiff:{$backup}";?>" >diff</a>]
-    </li>
+    <?php if (!empty($_siteObj->filerObj->backup_list)) { ?>
+    <div id="backUpList">
+        <ul>
+            <?php foreach ($_siteObj->filerObj->backup_list as $backup) { ?>
+            <li>
+                <?php echo $backup;?>:
+                [<a href="<?php echo "{$self}/_bkView:{$backup}";?>">view</a>]
+                [<a href="<?php echo "{$self}/_bkDiff:{$backup}";?>">diff</a>]
+            </li>
+            <?php } ?>
+        </ul>
+    </div>
     <?php } ?>
-  </ul>
-</div>
-      <?php }  ?>
-<?php endif; ?>
-<?php if( !empty( $debug ) ) { ?>
-<style>
-    div#debugInfo {
-        font-size: 12px;
-        color: #666666;
-        margin: 10px;
-        padding: 5px 5px 5px 25px;
-        border: 1px solid #cccccc;
-        box-shadow: 2px 2px 2px #cccccc;
-    }
-    div#debugInfo h3 {
-        margin: 0px;
-        padding: 2px;
-    }
-    div#debugInfo .debugTraceInfo {
-        float: left;
-        margin: 0px 0px 0px -20px;
-    }
-    div#debugInfo table {
-        border:1px solid gray; font-size: 11px; border-collapse: collapse;
-    }
-    div#debugInfo td,th { border: 1px dotted gray; vertical-align: top; }
-    div#debugInfo th { background-color: #E0E0E0; }
-</style>
-<div id='debugInfo'><?php echo $debug;?></div>
+    <?php endif; ?>
+<?php if (!empty($debug)) { ?>
+    <style>
+        div#debugInfo {
+            font-size: 12px;
+            color: #666666;
+            margin: 10px;
+            padding: 5px 5px 5px 25px;
+            border: 1px solid #cccccc;
+            box-shadow: 2px 2px 2px #cccccc;
+        }
+
+        div#debugInfo h3 {
+            margin: 0px;
+            padding: 2px;
+        }
+
+        div#debugInfo .debugTraceInfo {
+            float: left;
+            margin: 0px 0px 0px -20px;
+        }
+
+        div#debugInfo table {
+            border: 1px solid gray;
+            font-size: 11px;
+            border-collapse: collapse;
+        }
+
+        div#debugInfo td, th {
+            border: 1px dotted gray;
+            vertical-align: top;
+        }
+
+        div#debugInfo th {
+            background-color: #E0E0E0;
+        }
+    </style>
+    <div id='debugInfo'><?php echo $debug;?></div>
 </div>
 <?php }    // end of if on isset( $_siteObj->filerObj ) //  ?>
 </body>
