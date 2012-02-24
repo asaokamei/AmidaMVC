cbbacadd70130717c8ef476f1287ce669a4b6b61
Asao Kamei <kamei@workspot.jp>
2012-02-24 13:25:46 +0900

    added token management for CSRF.

--- src/AmidaMVC/Tools/Session.php
+++ src/AmidaMVC/Tools/Session.php
@@ -6,7 +6,18 @@ namespace AmidaMVC\Tools;
 
 class Session
 {
+    /**
+     * @var bool  flag to check if session started.
+     */
     static $session_start = FALSE;
+    /**
+     * @var null   temporary saves generated token. 
+     */
+    static $session_token = NULL;
+    /**  name of tokens stored in Session.     */
+    const  TOKEN_ID   = 'sess.token.ids';
+    /**  name of token send via post */
+    const  TOKEN_NAME = 'sessTokenValue_';
     // +-------------------------------------------------------------+
     static function start() {
         if( !static::$session_start ) {
@@ -31,6 +42,54 @@ class Session
     static function get( $name ) {
         if( isset( $_SESSION ) && array_key_exists( $name,  $_SESSION ) ) {
             return $_SESSION[ $name ];
+        }
+        return FALSE;
+    }
+    // +-------------------------------------------------------------+
+    //  managing token for CSRF. 
+    // +-------------------------------------------------------------+
+    static function pushToken() {
+        $token = md5( 'sess.dumb' . time() . $_SERVER["SCRIPT_FILENAME"] );
+        static::_pushToken( $token );
+        static::$session_token = $token;
+        return $token;
+    }
+    // +-------------------------------------------------------------+
+    static function _pushToken( $token ) {
+        static::start();
+        if( !isset( $_SESSION[ static::TOKEN_ID ] ) ) {
+            $_SESSION[ static::TOKEN_ID ] = array();
+        }
+        $max_token = 20;
+        $_SESSION[ static::TOKEN_ID ][] = $token;
+        if( count( $_SESSION[ static::TOKEN_ID ] ) > $max_token ) {
+            $num_remove = count( $_SESSION[ static::TOKEN_ID ] ) - $max_token;
+            $_SESSION[ static::TOKEN_ID ] =
+                array_slice( $_SESSION[ static::TOKEN_ID ], $num_remove );
+        }
+        if( WORDY ) wordy_table( $_SESSION[ static::TOKEN_ID ], 'token:'.static::TOKEN_ID );
+    }
+    // +-------------------------------------------------------------+
+    static function popToken() {
+        $name  = static::TOKEN_NAME;
+        $value = static::$session_token;
+        return "<input type=\"hidden\" name=\"{$name}\" value=\"{$value}\">";
+    }
+    // +-------------------------------------------------------------+
+    static function verifyToken() {
+        static::start();
+        $token = $_POST[ static::TOKEN_NAME ];
+        if( WORDY ) wordy_table( $_SESSION[ static::TOKEN_ID ], "token: $token" );
+        if( $token && !empty( $_SESSION[ static::TOKEN_ID ] ) ) {
+            if( in_array( $token, $_SESSION[ static::TOKEN_ID ] ) ) {
+                foreach( $_SESSION[ static::TOKEN_ID ] as $k=>$v ) {
+                    if( $v === $token ) {
+                        unset( $_SESSION[ static::TOKEN_ID ][$k] );
+                    }
+                }
+                $_SESSION[ static::TOKEN_ID ] = array_values( $_SESSION[ static::TOKEN_ID ] );
+                return TRUE;
+            }
         }
         return FALSE;
     }
