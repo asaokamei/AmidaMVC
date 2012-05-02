533419bf52abbee1f570b546a56ab2aefa4f9b26
asao Kamei <asao@ubuPC.(none)>
2012-05-02 14:11:51 +0900

    modified truePath to add trailing slash (/) when shortening a path to directory.

--- src/AmidaMVC/Tools/Request.php
+++ src/AmidaMVC/Tools/Request.php
@@ -79,6 +79,7 @@ class Request
     {
         // whether $path is unix or not
         $unipath=strlen($path)==0 || $path{0}!='/';
+        $lastSlash = ( substr($path, -1, 1 ) === '/' ) ? '/' : '';
         // attempts to detect if path is relative in which case, add cwd
         if(strpos($path,':')===false && $unipath)
             $path=getcwd().DIRECTORY_SEPARATOR.$path;
@@ -99,7 +100,7 @@ class Request
         if(file_exists($path) && linkinfo($path)>0)$path=readlink($path);
         // put initial separator that could have been lost
         $path=!$unipath ? '/'.$path : $path;
-        return $path;
+        return $path . $lastSlash;
     }
     // +-------------------------------------------------------------+
 }
