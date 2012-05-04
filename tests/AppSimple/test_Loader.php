50c240977d0c4b32a75efa2fbb45fac17042e4a5
kamei@workspot.jp <asao@asaoMBAir.local>
2012-05-04 14:15:50 +0900

    added bunches of more tests for Loader.

--- tests/AppSimple/test_Loader.php
+++ tests/AppSimple/test_Loader.php
@@ -12,12 +12,15 @@ class TestLoader extends \AmidaMVC\AppSimple\Loader
 
 class testCtrl
 {
+    var $loadInfo = array();
     var $redirectPath = NULL;
     var $commands = array();
+    var $action = 'defaultAction';
     function getPathInfo() {
         return 'test';
     }
-    function getLocation() {
+    function getLocation( $file=NULL ) {
+        if( isset( $file ) ) return 'loc/' . $file;
         return 'loc';
     }
     function redirect( $path ) {
@@ -25,6 +28,9 @@ class testCtrl
     }
     function getCommands() {
         return $this->commands;
+    }
+    function setAction( $action ) {
+        $this->action = $action;
     }
 }
 
@@ -45,7 +51,29 @@ class testPageObj {
     }
 }
 
-
+class loadTestClass {
+    /**
+     * @var \AmidaMVC\Tools\Load
+     */
+    static $_loadClass = '\AmidaMVC\Tools\Load';
+    static function getFileType( $file ) {
+        return pathinfo( $file, PATHINFO_EXTENSION );
+    }
+    static function isView( $file ) {
+        $class = self::$_loadClass;
+        return $class::isView( $file );
+    }
+    static function isAsIs( $file ) {
+        $class = self::$_loadClass;
+        return $class::isAsIs( $file );
+    }
+    static function getContentsByBuffer( $file ) {
+        return 'got contents by buffer...';
+    }
+    static function getContentsByGet( $file ) {
+        return 'got contntes by get...';
+    }
+}
 class test_AppSimple_Loader extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -57,7 +85,7 @@ class test_AppSimple_Loader extends \PHPUnit_Framework_TestCase
      */
     var $tLoader;
     /**
-     * @var \AmidaMVC\Framework\Controller
+     * @var testCtrl
      */
     var $_ctrl;
     /**
@@ -85,8 +113,21 @@ class test_AppSimple_Loader extends \PHPUnit_Framework_TestCase
         $this->assertNotEquals( $orig_loaderClass, $this->tLoader->getLoadClass() );
     }
     // +----------------------------------------------------------------------+
+    function test_defaultFileNotSet() {
+        $loadInfo = array(
+            'type' => 'markdown',
+            'file' => NULL, // testing when file is NOT SET.
+        );
+        $return = $this->loader->actionDefault(
+            $this->_ctrl,
+            $this->_pageObj,
+            $loadInfo );
+        $this->assertFalse( $return );
+    }
+    // +----------------------------------------------------------------------+
     function test_defaultClosure() {
         $loadInfo = array(
+            // setting file to be a closure.
             'file' => function() {return '#test closure'; },
             'type' => 'markdown',
         );
@@ -96,6 +137,7 @@ class test_AppSimple_Loader extends \PHPUnit_Framework_TestCase
             $loadInfo );
         $this->assertTrue( $return );
 
+        /** @var $closure closure */
         $closure = $loadInfo['file'];
         $orig_content = $closure();
         $test_content = $this->_pageObj->getContent();
@@ -103,6 +145,101 @@ class test_AppSimple_Loader extends \PHPUnit_Framework_TestCase
 
         $loadMode = $this->_ctrl->loadInfo[ 'loadMode' ];
         $this->assertEquals( '_closure', $loadMode );
+    }
+    // +----------------------------------------------------------------------+
+    function test_defaultFile() {
+        $option = array(
+            'loadClass' => '\AmidaMVC\Tests\AppSimple\testLoader\loadTestClass',
+        );
+        $this->loader->_init( $option );
+        $loadInfo = array(
+            'file' => 'file.md',
+        );
+        $return = $this->loader->actionDefault(
+            $this->_ctrl,
+            $this->_pageObj,
+            $loadInfo );
+        $this->assertTrue( $return );
+
+        $orig_content = loadTestClass::getContentsByBuffer('file');
+        $test_content = $this->_pageObj->getContent();
+        $this->assertEquals( $orig_content, $test_content );
+
+        $loadMode = $this->_ctrl->loadInfo[ 'loadMode' ];
+        $this->assertEquals( '_view', $loadMode );
+    }
+    // +----------------------------------------------------------------------+
+    function test_defaultFileSrc() {
+        $option = array(
+            'loadClass' => '\AmidaMVC\Tests\AppSimple\testLoader\loadTestClass',
+        );
+        $this->loader->_init( $option );
+        $loadInfo = array(
+            'file' => 'file.md',
+        );
+        // set _src commands to get content as is
+        $this->_ctrl->commands = array( '_src' );
+        $return = $this->loader->actionDefault(
+            $this->_ctrl,
+            $this->_pageObj,
+            $loadInfo );
+        $this->assertTrue( $return );
+
+        // getting the contents by get, appropriate method for _src.
+        $orig_content = loadTestClass::getContentsByGet('file');
+        $test_content = $this->_pageObj->getContent();
+        $this->assertEquals( $orig_content, $test_content );
+
+        $loadMode = $this->_ctrl->loadInfo[ 'loadMode' ];
+        $this->assertEquals( '_src', $loadMode );
+    }
+    // +----------------------------------------------------------------------+
+    function test_defaultAsIs() {
+        $option = array(
+            'loadClass' => '\AmidaMVC\Tests\AppSimple\testLoader\loadTestClass',
+        );
+        $this->loader->_init( $option );
+        $loadInfo = array(
+            'file' => 'file.jpg',
+        );
+        $return = $this->loader->actionDefault(
+            $this->_ctrl,
+            $this->_pageObj,
+            $loadInfo );
+        $this->assertTrue( $return );
+
+        $orig_content = loadTestClass::getContentsByGet('file');
+        $test_content = $this->_pageObj->getContent();
+        $this->assertEquals( $orig_content, $test_content );
+
+        $loadMode = $this->_ctrl->loadInfo[ 'loadMode' ];
+        $this->assertEquals( '_asIs', $loadMode );
+    }
+    // +----------------------------------------------------------------------+
+    function test_defaultFileAndAction() {
+        $option = array(
+            'loadClass' => '\AmidaMVC\Tests\AppSimple\testLoader\loadTestClass',
+        );
+        $this->loader->_init( $option );
+        $loadInfo = array(
+            'file' => 'file.text',
+            'action' => 'testingAction'
+        );
+        $orig_action  = $this->_ctrl->action;
+        $return = $this->loader->actionDefault(
+            $this->_ctrl,
+            $this->_pageObj,
+            $loadInfo );
+        $this->assertTrue( $return );
+
+        $orig_content = loadTestClass::getContentsByBuffer('file');
+        $test_content = $this->_pageObj->getContent();
+        $this->assertEquals( $orig_content, $test_content );
+
+        $loadMode = $this->_ctrl->loadInfo[ 'loadMode' ];
+        $this->assertEquals( '_view', $loadMode );
+        $this->assertEquals( $loadInfo[ 'action' ], $this->_ctrl->action );
+        $this->assertNotEquals( $orig_action, $this->_ctrl->action );
     }
     // +----------------------------------------------------------------------+
 }
