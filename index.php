1d0f07a4efbb84d61fb41aa795a0a8e0474a4988
kamei@workspot.jp <asao@asaoMBAir.local>
2012-05-04 17:04:29 +0900

    use template at demo1-simple/template.php.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once( __DIR__ . '/src/AmidaMVC/bootstrap.php' );
 $app = \AmidaMVC\AppSimple\Application::getInstance(
     array(
         'site_title' => "AmidaMVC PHP Framework",
+        'template_file' => '/demo1-simple/template.php'
     )
 );
 $app->start();
