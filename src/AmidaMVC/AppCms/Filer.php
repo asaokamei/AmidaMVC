f2acf8d54c7affe57733f5d29f63de5f83f70076
kamei@workspot.jp <asao@asaoMBAir.local>
2012-05-06 16:19:14 +0900

    added bootstrap js and css when filer activated; added comments.

--- src/AmidaMVC/AppCms/Filer.php
+++ src/AmidaMVC/AppCms/Filer.php
@@ -3,11 +3,27 @@ namespace AmidaMVC\AppCms;
 
 class Filer implements \AmidaMVC\Framework\IModule
 {
+    // +-------------------------------------------------------------+
+    /**
+     * @param array $option
+     */
     function _init( $option=array() ) {
     }
+    // +-------------------------------------------------------------+
+    /**
+     * @param \AmidaMVC\AppSimple\Application $_ctrl
+     * @param \AmidaMVC\Framework\PageObj $_pageObj
+     * @param array $option
+     * @return array
+     */
     function actionDefault( $_ctrl, &$_pageObj, $option=array() )
     {
+        $_pageObj->setJs(  '../bootstrap/js/jquery-1.7.1.js' );
+        $_pageObj->setJs(  '../bootstrap/js/bootstrap.js' );
+        $_pageObj->setJs(  '../bootstrap/js/bootstrap-modal.js' );
+        $_pageObj->setCss( '../bootstrap/css/bootstrap.css' );
         return $option;
     }
+    // +-------------------------------------------------------------+
 }
 
