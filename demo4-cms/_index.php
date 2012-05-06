ed1e950b0f2247fae25c00aaf16c31579e7a8ea5
kamei@workspot.jp <asao@asaoMBAir.local>
2012-05-06 12:09:44 +0900

    reports all the error.

--- demo4-cms/_index.php
+++ demo4-cms/_index.php
@@ -1,6 +1,8 @@
 <?php
 require_once( __DIR__ . '/../src/AmidaMVC/bootstrap.php' );
 
+error_reporting( E_ALL );
+
 $app = \AmidaMVC\AppCms\Application::getInstance(
     array(
         'site_title' => "Demo#4 - like a CMS",
