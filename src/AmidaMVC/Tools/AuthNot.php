6af59b36efba9edbccb07d5dca4c7f41f7f4408c
kamei@workspot.jp <asao@asaoMBAir.local>
2012-05-06 10:07:38 +0900

    Auth* class is instanciated, not static anymore.

--- src/AmidaMVC/Tools/AuthNot.php
+++ src/AmidaMVC/Tools/AuthNot.php
@@ -7,62 +7,70 @@ namespace AmidaMVC\Tools;
 
 class AuthNot
 {
-    static $auth_id    = 'AuthNot_id';
-    static $login_name = 'auth_name';
-    static $login_pass = 'auth_pass';
-    static $act_name   = 'auth_act';
-    static $act_value  = 'authNot';
-    static $auth_info  = array();
-    static $option     = array();
+    var $auth_id    = 'AuthNot_id';
+    var $login_name = 'auth_name';
+    var $login_pass = 'auth_pass';
+    var $act_name   = 'auth_act';
+    var $act_value  = 'authNot';
+    var $auth_info  = array();
+    var $option     = array();
     // +-------------------------------------------------------------+
-    static function _init( $option=array() ) {
-        static::$option = $option;
+    static function getInstance( $authArea ) {
+        static $self = array();
+        if( !isset( $self[ $authArea ] ) ) {
+            $self[ $authArea ] = new self();
+        }
+        return $self[ $authArea ];
     }
     // +-------------------------------------------------------------+
-    static function getAuth() {
+    function _init( $option=array() ) {
+        $this->option = $option;
+    }
+    // +-------------------------------------------------------------+
+    function getAuth() {
         if( $auth_info = static::_verifySession() ) {
             $auth_info[ 'auth_method' ] = 'session';
-            static::$auth_info = $auth_info;
+            $this->auth_info = $auth_info;
         }
         elseif( $auth_info = static::_verifyPost() ) {
             $auth_info[ 'auth_method' ] = 'post';
-            static::$auth_info = $auth_info;
+            $this->auth_info = $auth_info;
             static::_saveSession( $auth_info );
         }
         return $auth_info;
     }
     // +-------------------------------------------------------------+
-    static function logout(){
+    function logout(){
         Session::start();
-        Session::del( static::$auth_id );
+        Session::del( $this->auth_id );
     }
     // +-------------------------------------------------------------+
-    static function _verifyPost() {
+    function _verifyPost() {
         if( !isset( $_POST ) || 
-            !isset( $_POST[ static::$act_name ] ) || 
-            !isset( $_POST[ static::$login_name ] ) ||
-            !isset( $_POST[ static::$login_pass ] ) ) {
+            !isset( $_POST[ $this->act_name ] ) ||
+            !isset( $_POST[ $this->login_name ] ) ||
+            !isset( $_POST[ $this->login_pass ] ) ) {
             return FALSE;
         }
-        if( $_POST[ static::$act_name ] !== static::$act_value ) return FALSE;
-        if( $_POST[ static::$login_name ] !== $_POST[ static::$login_pass ] ) return FALSE;
+        if( $_POST[ $this->act_name ] !== $this->act_value ) return FALSE;
+        if( $_POST[ $this->login_name ] !== $_POST[ $this->login_pass ] ) return FALSE;
         $auth_info = array(
-            static::$login_name => $_POST[ static::$login_name ],
-            static::$login_pass => $_POST[ static::$login_pass ],
-            static::$act_name   => $_POST[ static::$act_name ],
+            $this->login_name => $_POST[ $this->login_name ],
+            $this->login_pass => $_POST[ $this->login_pass ],
+            $this->act_name   => $_POST[ $this->act_name ],
         );
         return $auth_info;
     }
     // +-------------------------------------------------------------+
-    static function _saveSession( $auth_info ) {
-        Session::set( static::$auth_id, $auth_info );
+    function _saveSession( $auth_info ) {
+        Session::set( $this->auth_id, $auth_info );
     }
     // +-------------------------------------------------------------+
-    static function _verifySession() {
+    function _verifySession() {
         Session::start();
-        $auth_info = Session::get( static::$auth_id );
+        $auth_info = Session::get( $this->auth_id );
         if( $auth_info && 
-            $auth_info[ static::$act_name ] == static::$act_value ) {
+            $auth_info[ $this->act_name ] == $this->act_value ) {
             return $auth_info;
         }
         return FALSE;
