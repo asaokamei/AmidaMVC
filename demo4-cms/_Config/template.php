12bf8a0533d8870a0df20c66ff3eb5a70f34df7b
asao Kamei <asao@ubuPC.(none)>
2012-05-08 14:36:10 +0900

    show JavaScript at the beginning of page. less hassle.

--- demo4-cms/_Config/template.php
+++ demo4-cms/_Config/template.php
@@ -9,6 +9,7 @@ $_pageObj->setJs( '../bootstrap/js/jquery-1.7.1.js' );
 <title><?php echo $_ctrl->getOption( 'site_title' ); ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php echo $_pageObj->getCssLinks( $_ctrl ); ?>
+<?php echo $_pageObj->getJsLinks( $_ctrl ); ?>
 <div class="mainbody">
     <header><a href="<?php echo $_ctrl->getBaseUrl(); ?>"><?php echo $_ctrl->getOption( 'site_title' ); ?></a></header>
 
@@ -29,7 +30,6 @@ $_pageObj->setJs( '../bootstrap/js/jquery-1.7.1.js' );
         <p>AppSimple Suites by AmidaMVC.<br />
         gitHub:https://github.com/asaokamei/AmidaMVC</p>
     </footer>
-    <?php echo $_pageObj->getJsLinks( $_ctrl ); ?>
     <script type="text/javascript">
         $(document).ready( function() {
             $( "table" ).addClass( "table" );
