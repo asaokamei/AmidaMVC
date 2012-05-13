991d348fe2dba8b7473d98027a0a19b733c20da5
kamei@workspot.jp <asao@asaoMBAir.local>
2012-05-13 16:51:05 +0900

    brain dump on new di options using AppSimple.

--- src/AmidaMVC/AppSimple/Application.php
+++ src/AmidaMVC/AppSimple/Application.php
@@ -10,6 +10,24 @@ class Application extends \AmidaMVC\Framework\Controller
      */
     function __construct( $option=array() )
     {
+        $moduleDefault = array(
+            array( 'router',         'router' ),
+            array( 'loader',         'loader' ),
+            array( 'emitter',        'emitter' ),
+        );
+        $diDefault = array(
+            array( 'router',    '\AmidaMVC\AppSimple\Router', 'new',
+                array( 'routes' => array() )
+            ),
+            array( 'loader',    '\AmidaMVC\AppSimple\Loader', 'new', array() ),
+            array( 'emitter',   '\AmidaMVC\AppSimple\Emitter', 'new', array() ),
+        );
+        $ctlDefault = array(
+            'site_title' => 'AppSimple Web Site',
+            'template_file' => NULL,
+            'pageNotFound_file' => FALSE,
+            'appDefault' => NULL,
+        );
         $default = array(
             'site_title' => 'AppSimple Web Site',
             'template_file' => NULL,
