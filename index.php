f32269701c08f1f9817917a99b9c193a7fcb5293
asao Kamei <asao@ubuPC.(none)>
2012-05-14 10:48:51 +0900

    use Application::simple for serving.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once( __DIR__ . '/src/AmidaMVC/bootstrap.php' );
 
-$app = \AmidaMVC\AppSimple\Application::getInstance(
+$app = \AmidaMVC\Application\Application::simple(
     array(
         'site_title' => "AmidaMVC PHP Framework",
         'template_file' => '/demo1-simple/template.php'
