5e94698ce4d3c914a0798ff234f507bac16263b8
kamei@workspot.jp <asao@asaoMBAir.local>
2012-05-17 06:46:35 +0900

    created demo2-array, going to store all file info in an array.

---
+++ demo2-array/_index.php
@@ -0,0 +1,24 @@
+<?php
+require_once( __DIR__ . '/../src/AmidaMVC/bootstrap.php' );
+
+$app = \AmidaMVC\Application\Application::simple(
+    array(
+        'site_title' => "Demo#2 - Simple Site with Template",
+        'template_file' => 'template.php',
+        'ctrl_root' => realpath( '../demo1-simple' ),
+    )
+);
+$app->get( '/func', function() {
+    $content = "
+#Closure Output
+
+AmidaMVC can handle closure function to generate a response;
+this text is an output from closure in MarkDown description.
+
+Just in case you may notice, this page uses template with demo.css
+in demo1-simple folder.
+";
+        return $content;
+    }, array( 'type' => 'markdown' )
+);
+$app->start();
