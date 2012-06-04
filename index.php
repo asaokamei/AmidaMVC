9a657a2968119f5f23f899941bfac6b2d2aa0def
asao Kamei <asao@ubuPC.(none)>
2012-06-04 16:40:38 +0900

    trying to make index.php work, but does not.

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 require_once( __DIR__ . '/src/AmidaMVC/bootstrap.php' );
 
-$app = \AmidaMVC\Application\Application::simple(
-    array(
-        'site_title' => "AmidaMVC PHP Framework",
-        'template_file' => '/demo1-simple/template.php'
-    )
-);
-$app->start();
+$app = \AmidaMVC\Application\App2::app();
+$app
+    ->setOption( 'site_title', 'AmidaMVC PHP Framework' )
+    ->start();
