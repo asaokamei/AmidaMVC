d5155c08a1761e3102633a409eaf4998671b068c
asao Kamei <asao@ubuPC.(none)>
2012-06-15 14:19:06 +0900

    simplified arguments by keeping _ctrl and _pageObj as object properties.

--- src/AmidaMVC/Module/Auth.php
+++ src/AmidaMVC/Module/Auth.php
@@ -18,6 +18,10 @@ class Auth implements IfModule
     );
     var $option = array();
     var $_evaluateOn = array();
+    /** @var \AmidaMVC\Framework\Controller */
+    protected $_ctrl;
+    /** @var \AmidaMVC\Framework\PageObj */
+    protected $_pageObj;
     // +-------------------------------------------------------------+
     /**
      * initialize class.
@@ -49,13 +53,15 @@ class Auth implements IfModule
      */
     function actionDefault( $_ctrl, &$_pageObj, $option=array() )
     {
+        $this->_ctrl = $_ctrl;
+        $this->_pageObj = $_pageObj;
         if( empty( $this->_evaluateOn ) ) {
             return TRUE;
         }
         // do the authentication
-        if( $this->matchPathInfo( $_ctrl, $this->_evaluateOn[ 'onPathInfo' ] ) ) {
+        if( $this->matchPathInfo( $this->_evaluateOn[ 'onPathInfo' ] ) ) {
             // set up auth class as well.
-            $this->_auth = $_ctrl->getServices()->get( $this->authArea );
+            $this->_auth = $this->_ctrl->getServices()->get( $this->authArea );
             $auth_success = $this->_auth->getAuth();
             if( $auth_success ) {
                 $doList = $this->_evaluateOn[ 'onSuccess' ];
@@ -63,7 +69,7 @@ class Auth implements IfModule
             else {
                 $doList = $this->_evaluateOn[ 'onFail' ];
             }
-            $this->doList( $_ctrl, $_pageObj, $doList );
+            $this->doList( $doList );
         }
         return TRUE;
     }
@@ -76,17 +82,15 @@ class Auth implements IfModule
      */
     function action_loginForm( $_ctrl, &$_pageObj, $option=array() )
     {
-        // generally, do nothing for loginForm action in auth.
         return TRUE;
     }
     // +-------------------------------------------------------------+
     /**
-     * @param \AmidaMVC\Framework\Controller $_ctrl
      * @param array|string $path
      * @return bool
      */
-    function matchPathInfo( $_ctrl, $path ) {
-        $pathInfo = '/' . $_ctrl->getPathInfo();
+    function matchPathInfo( $path ) {
+        $pathInfo = '/' . $this->_ctrl->getPathInfo();
         if( !is_array( $path ) ) {
             $path = array( $path );
         }
@@ -100,70 +104,56 @@ class Auth implements IfModule
     }
     // +-------------------------------------------------------------+
     /**
-     * @param \AmidaMVC\Framework\Controller $_ctrl
-     * @param \AmidaMVC\Framework\PageObj $_pageObj
      * @param array $doList
      * @return bool
      */
-    function doList( $_ctrl, $_pageObj, $doList ) {
+    function doList( $doList ) {
         if( empty( $doList ) ) return TRUE;
         foreach( $doList as $method => $parameter ) {
-            $args = array( $_ctrl, $_pageObj );
-            if( is_array( $parameter ) ) {
-                $args = array_merge( $args, $parameter );
+            if( !is_array( $parameter ) ) {
+                $parameter = array( $parameter );
             }
-            else {
-                $args[] = $parameter;
-            }
-            call_user_func_array( array( $this, $method ), $args );
+            call_user_func_array( array( $this, $method ), $parameter );
         }
         return TRUE;
     }
     // +-------------------------------------------------------------+
     /**
-     * @param \AmidaMVC\Framework\Controller $_ctrl
-     * @param \AmidaMVC\Framework\PageObj     $_pageObj
      * @param string                          $url
      * @return bool
      */
-    function redirect( $_ctrl, $_pageObj, $url ) {
-        $_ctrl->redirect( $url );
+    function redirect( $url ) {
+        $this->_ctrl->redirect( $url );
         return TRUE;
     }
     // +-------------------------------------------------------------+
     /**
-     * @param \AmidaMVC\Framework\Controller $_ctrl
-     * @param \AmidaMVC\Framework\PageObj $_pageObj
-     * @param string $message
+     * @param $login_file
      * @return bool
      */
-    function setLoginForm( $_ctrl, $_pageObj, $login_file ) {
+    function setLoginForm( $login_file ) {
         // read login form template.
         // how can I pass a parameter/message to the loader???
-        $_ctrl->setAction( '_loginForm' );
-        $_ctrl->options[ 'loginForm_file' ] = $login_file;
+        $this->_ctrl->setAction( '_loginForm' );
+        $this->_ctrl->options[ 'loginForm_file' ] = $login_file;
         return TRUE;
     }
     // +-------------------------------------------------------------+
     /**
-     * @param \AmidaMVC\Framework\Controller $_ctrl
-     * @param \AmidaMVC\Framework\PageObj $_pageObj
      * @param $after
      * @param $module
      * @param $name
      * @return bool
      */
-    function addModuleAfter( $_ctrl, $_pageObj, $after, $module, $name ) {
-        $_ctrl->addModuleAfter( $after, $module, $name );
+    function addModuleAfter( $after, $module, $name ) {
+        $this->_ctrl->addModuleAfter( $after, $module, $name );
         return TRUE;
     }
     // +-------------------------------------------------------------+
     /**
-     * @param \AmidaMVC\Framework\Controller $_ctrl
-     * @param \AmidaMVC\Framework\PageObj $_pageObj
      * @param $message
      */
-    function logout( $_ctrl, $_pageObj, $message ) {
+    function logout( $message ) {
         $this->_auth->logout();
     }
     // +-------------------------------------------------------------+
