c4613cfff25fbaeaec812ef3c3726c1dea4f0b0e
Asao <kamei@workspot.jp>
2012-06-16 11:27:35 +0900

    set the ctrl_root to the current folder.

--- _index.php
+++ _index.php
@@ -2,6 +2,7 @@
 require_once( __DIR__ . '/src/AmidaMVC/bootstrap.php' );
 
 $app = \AmidaMVC\Application\App2::app();
+$app->setCtrlRoot( __DIR__ );
 $app
     ->setOption( 'site_title', 'AmidaMVC PHP Framework' )
     ->start();
