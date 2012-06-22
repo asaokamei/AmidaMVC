27af782a436fbbc9a7ba54442e14fa0553dc8dc2
asao Kamei <asao@ubuPC.(none)>
2012-06-22 13:30:13 +0900

    README.md is one of index files (along with index.php, etc.)

--- _index.php
+++ _index.php
@@ -3,6 +3,7 @@ require_once( __DIR__ . '/src/AmidaMVC/bootstrap.php' );
 
 $app = \AmidaMVC\Application\App2::app();
 $app->setCtrlRoot( __DIR__ );
+$app->setModuleOption( 'router', array( 'indexes' => 'README.md' ) );
 $app
     ->setOption( 'site_title', 'AmidaMVC PHP Framework' )
     ->start();
