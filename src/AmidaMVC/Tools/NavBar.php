897dbafc0a40d271c08bf97994bbd50280012cb5
asao Kamei <asao@ubuPC.(none)>
2012-07-03 14:59:37 +0900

    added space between icon and title.

--- src/AmidaMVC/Tools/NavBar.php
+++ src/AmidaMVC/Tools/NavBar.php
@@ -107,7 +107,7 @@ class NavBar
             $title = ( isset( $item[ 'title' ] ) ) ? $item[ 'title' ] : '';
             $score = ( isset( $item[ 'score' ] ) ) ? $item[ 'score' ] : 0;
             $active = ( $score >= $this->max_score ) ? ' active' : '';
-            if( isset( $item[ 'icon' ] ) ) $title = '<i class="icon-' . $item['icon'] . '"></i>' . $title;
+            if( isset( $item[ 'icon' ] ) ) $title = '<i class="icon-' . $item['icon'] . '"></i> ' . $title;
             if( isset( $item[ 'pages' ] ) ) {
                 $sub = $this->_ul( $item['pages'], 'subUl' );
                 $html .= $this->_text( 'liSub', $title, $sub, $active );
